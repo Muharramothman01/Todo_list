908d9196d21132b886e0eb6312353dceae79e427
Muharram Othman <68170632+Muharramothman01@users.noreply.github.com>
2021-08-31 15:49:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function createMission()
     
 
     todoList.appendChild(Mission);
-    Mission.classList.add('mission','col-xxl-12','col-10','my-4');
+    Mission.classList.add('mission','col-xxl-12','col-10','my-4',"undone");
     
     Mission.appendChild(content_todo);
     val.setAttribute('value',val.value);
@@ -48,6 +48,8 @@ function completeFunc(event)
         if(CompId === document.querySelectorAll('.comp')[i].id)
         {
             list[i].style.cssText = "background-color:rgb(100, 100, 100); text-decoration: line-through;";
+            list[i].classList.remove("undone");
+            list[i].classList.add("done");
             break;
         }
         else
@@ -71,10 +73,64 @@ function deleteFunc (event)
         else
             continue;
     }
-
 }
+
+   
+function selectFunc()
+{
+    if(document.querySelectorAll('option')[2].selected)
+    {
+        var list = document.querySelectorAll('.mission');
+        for(var i = 0 ; i < list.length ;i++) 
+        {
+            if(list[i].classList[4] === "undone")
+            {
+                list[i].style.cssText ="display:none;";
+            }
+            else
+            {
+                list[i].style.cssText ="display:block; background-color:rgb(100, 100, 100); text-decoration: line-through;";
+            }
+        }
+    }
+
+    else if(document.querySelectorAll('option')[1].selected)
+    {
+        var list = document.querySelectorAll('.mission');
+        for(var i = 0 ; i < list.length ;i++)
+        {
+            if(list[i].classList[4] === "done")
+            {
+                list[i].style.cssText ="display:none;";
+            }
+            else
+            {
+                list[i].style.cssText ="display:block;";
+            }
+        }
+    }
+
+    else if(document.querySelectorAll('option')[0].selected)
+    {
+        var list = document.querySelectorAll('.mission');
+        for(var i = 0 ; i < list.length ;i++)
+        {
+            if(list[i].classList[4] === "done")
+            {
+                list[i].style.cssText ="display:block; background-color:rgb(100, 100, 100); text-decoration: line-through;";
+            }
+
+            else if(list[i].classList[4] === "undone")
+            {
+                list[i].style.cssText ="display:block;";
+            }
+        }
+    }
+}
+    
 
 
 addition.addEventListener("click",addMission);
 todoList.addEventListener("click",completeFunc); 
 todoList.addEventListener('click',deleteFunc)
+document.querySelector('select').addEventListener('click',selectFunc)
